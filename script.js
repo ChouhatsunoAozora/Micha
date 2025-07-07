8fb41e2fc9b93133eb211186011ba9358028331a
ChouhatsunoAozora <6611110142@mut.ac.th>
2025-07-07 11:33:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 function tweetMessage() {
-    const message = "Micha Chouhatsu no Aozora\n#MichaBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay1";
+    const message = "Micha ChouhatsunoAozora\n#MichaBNK48\n#คำท้าทายที่ปลายฟ้าConceptDay1";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 }
